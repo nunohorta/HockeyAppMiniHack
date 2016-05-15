a6e88d1d913b5fb04a87a0457e21e69134318de7
Nuno Horta <nunohorta@Nunos-MacBook-Pro.local>
2016-05-15 15:28:00 +0100

    new project for Xamarin Mini Hack

---
+++ HockeyAppMiniHack/ViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using UIKit;
+
+namespace HockeyAppMiniHack
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		object nullObject = null;
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			CrashButton.TouchUpInside += (object sender, EventArgs e) => {
+				nullObject.ToString();
+			};
+		}
+	}
+}
+
